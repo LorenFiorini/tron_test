3bb279a11b5790e682de4b7ca002811185dd793d
Lorenzo Fiorini <lorenfiorini@gmail.com>
2022-07-11 23:48:36 +0200

    js & ignore

--- docs/js/app.js
+++ docs/js/app.js
@@ -9,6 +9,7 @@ try {
       metacoinConfig.fullHost,
       metacoinConfig.privateKey
   )
+  tronWeb.setHeader({ "TRON-PRO-API-KEY": '42530830-83f7-4236-8093-30aa835d9517' });
 } catch (err) {
   alert('The app looks not configured. Please run `npm run migrate`')
 }
---
+++ docs/js/metacoin-config.js
@@ -0,0 +1,9 @@
+
+
+var metacoinConfig = {
+    contractAddress: 'TCyKydExSAApBFLYVyWs7G5Ye3z9F4AJpG',
+    fullhost: 'https://api.shasta.trongrid.io',
+    privateKey: '92e14dfa26611ce1c425eb14ef2cf3697f64e4162625f529a4fb9ace829f1770',
+    apiKey: '42530830-83f7-4236-8093-30aa835d9517',
+}
+
